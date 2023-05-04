4226677415839f20b28a2d3deee5f085ff1f5b9a
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-04 15:50:36 +0700

    add BookDto

---
+++ src/Abp.Acme.BookStore.Application.Contracts/BookDto/BookDto.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Acme.BookStore.Books;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace Abp.Acme.BookStore.BookDto
+{
+    public class BookDto: AuditedEntityDto<Guid>
+    {
+        public string Name { get; set; }
+        public BookType Type { get; set; }
+
+        public DateTime PublishDate { get; set; }
+        public float Price { get; set; }
+    }
+}
